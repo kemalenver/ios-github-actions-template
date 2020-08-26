6b09de6a52df458beaff28fbe1df924352a1d2ca
Kemal Enver <kemal.enver@qantasloyalty.com>
2020-08-26 15:09:52 +1000

    Test

---
+++ xcode-actions-setup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xcode-actions-setup
+//
+//  Created by Kemal Enver on 26/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
